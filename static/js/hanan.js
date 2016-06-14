4367a5e24ab697247e4dddef70e0ee443b7ddf1a
an.han <an.han@qunar.com>
2016-06-14 08:49:20 +0800

    done

---
+++ static/js/hanan.js
@@ -0,0 +1,14 @@
+/**
+ * Created by an.han on 16/6/13.
+ */
+
+var $ = require('components/jquery/jquery.js');
+var baidu = require('components/baidu-template.js');
+var _alert = require('components/alert/alert.js');
+require('components/hello.js');
+
+setTimeout(function () {
+    _alert({
+        content: 'aaaaaa'
+    });
+}, 1000);
